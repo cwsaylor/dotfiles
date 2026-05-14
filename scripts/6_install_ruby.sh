ba8433f41ee1c9d7bede47ee0fcc858ea7a269a0
Christopher Saylor <cwsaylor@gmail.com>
2026-05-14 09:57:54 -0700

    Update ruby to 4.04

--- scripts/6_install_ruby.sh
+++ scripts/6_install_ruby.sh
@@ -3,6 +3,6 @@ set -e
 
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
-rbenv install 3.4.8
-rbenv global 3.4.8
+rbenv install 4.0.4
+rbenv global 4.0.4
 gem install rails
